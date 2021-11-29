0228ed2241b891b7c7e0e5a3ae0044a7d1b9d9bf
向林 <as2252258@163.com>
2021-11-29 11:44:03 +0800

    改名

---
+++ .phpstorm.meta.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PHPSTORM_META {
+
+    // Reflect
+	use Kiri\Di\Container;
+	use Psr\Container\ContainerInterface;
+
+	override(ContainerInterface::get(0), map('@'));
+	override(Container::get(0), map('@'));
+	override(Container::make(0), map('@'));
+	override(Container::create(0), map('@'));
+//    override(\Hyperf\Utils\Context::get(0), map('@'));
+//    override(\make(0), map('@'));
+    override(\di(0), map('@'));
+    override(\duplicate(0), map('@'));
+
+}
