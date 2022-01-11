febbdea8c84914edc3a1d971e0651d662dc0f5e1
向林 <as2252258@163.com>
2022-01-11 14:44:02 +0800

    Revert "改名"
    This reverts commit fdf58326

--- RpcManager.php
+++ RpcManager.php
@@ -31,6 +31,7 @@ class RpcManager extends Component
 		if ($lists->getStatusCode() != 200) {
 			return;
 		}
+		var_dump($lists->getBody());
 		$body = json_decode($lists->getBody(), true);
 		$file = storage('.rpc.clients.' . md5($serviceName), 'rpc');
 		if (!empty($body) && is_array($body)) {
