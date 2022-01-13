89c142930eaab4a1f9929a282a9f1b713e33c7bf
向林 <as2252258@163.com>
2022-01-13 18:29:51 +0800

    Revert "改名"
    This reverts commit fdf58326

--- RpcManager.php
+++ RpcManager.php
@@ -143,11 +143,10 @@ class RpcManager extends Component
 	public function register()
 	{
 		$agent = Kiri::getDi()->get(Agent::class);
-
-		var_dump($agent);
 		foreach ($this->_rpc as $list) {
 			$agent->service->deregister($list['config']['ID']);
 			$data = $agent->service->register($list['config']);
+			var_dump($data);
 			if ($data->getStatusCode() != 200) {
 				return;
 			}
